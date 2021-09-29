bbc68667f7e06acaf6ba1cbba039ed2cfd139c47
Miguelantorr <55844593+Miguelantorr@users.noreply.github.com>
2021-09-29 17:57:58 +0200

    Agregar archivos de proyecto.

---
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AumentarContador(  ) 
+        {
+            String caracteres = caja.Text.Length.ToString();
+            contador.Text = caracteres + "/140";
+        }
+
+        private void BloquearCaja()
+        {
+            if (caja.Text.Length == 140)
+            {
+                caja.IsEnabled = false;
+            }
+        }
+
+        private void caja_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            caja_TextChanged(sender, e);
+            AumentarContador();
+            BloquearCaja();
+        }
+    }
+}
